2432bf33d61e86868cece208c46a20e92afa3183
Jhon <jhoncalsina51@gmail.com>
2023-12-01 16:31:37 -0400

    se modifica tamaño de modal en movil

--- portfolio/src/components/auth/AboutAuth.jsx
+++ portfolio/src/components/auth/AboutAuth.jsx
@@ -7,13 +7,13 @@ export default function App() {
   return (
     <>
       <Button onPress={onOpen} className="bg-[#9ec5e9da] dark:bg-[#35383a] flex items-center justify-center text-white dark:text-[#afabc2] font-bold gap-3 bg-opacity-80">Service offers</Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="2xl" backdrop="blur">
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="2xl" backdrop="blur" className="p-5 font-custom tracking-wide mx-6 sm:m-0">
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Service offers</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1 text-2xl">Service offers</ModalHeader>
               <ModalBody>
-               <div className="flex flex-row">
+               <div className="flex flex-row font-thin">
                   <p>I'm a specialized frontend developer with expertise in HTML, CSS, and JavaScript, along with experience in frameworks like Tailwind and React. I also have skills in logo design and effective collaboration in multidisciplinary teams. My focus is on creating responsive and intuitive web experiences. I'm committed to continuous learning and expanding my skills into backend development. My goal is to deliver innovative and functional digital solutions for each project.</p>
                </div>
               </ModalBody>
