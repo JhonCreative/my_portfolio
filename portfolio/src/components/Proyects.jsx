3571b851f5d5ba062471e259a49429c078abfffa
Jhon <jhoncalsina51@gmail.com>
2023-12-05 20:35:52 -0400

    se arregla projects

--- portfolio/src/components/Proyects.jsx
+++ portfolio/src/components/Proyects.jsx
@@ -2,18 +2,19 @@
 import React from 'react';
 import ProjectAuth from './auth/ProjectAuth';
 import img_artisys from '../assets/images/img/Project_artisys.png';
+import img_Licor from '../assets/images/img/Licor_koa.png'
 
 const projects = [
   {
     title: 'Project Artisys',
     description: 'Artysis simplifica la gestión de proyectos, permitiéndote enfocarte en tus tareas y creatividad de manera eficiente.',
-    imageUrl: {img_artisys},
+    imageUrl: img_artisys,
     link: 'https://example.com/project1',
   },
   {
-    title: 'Project 2',
-    description: 'Description for Project 2.',
-    imageUrl: 'https://placekitten.com/300/200',
+    title: 'Licor de K0A',
+    description: 'rtysis simplifica la gestión de proyectos, permitiéndote enfocarte en tus tareas y creatividad de manera eficiente.',
+    imageUrl: img_Licor,
     link: 'https://example.com/project2',
   },
   // Add more projects as needed
@@ -22,7 +23,7 @@ const projects = [
 const Proyects = () => {
   return (
    <section className='mx-auto max-w-3xl px-5 mb-5 flex flex-col gap-5 font-custom'>
-      <h1 className='text-2xl sm:text-1xl text-center sm:text-left font-bold text-slate-800 dark:text-slate-100'>More about me</h1>
+      <h1 className='text-2xl sm:text-1xl text-center sm:text-left font-bold text-slate-800 dark:text-slate-100'>My Projects</h1>
       <div className="flex flex-col gap-10">
          {projects.map((project, index) => (
          <ProjectAuth key={index} {...project} />
--- portfolio/src/components/auth/ProjectAuth.jsx
+++ portfolio/src/components/auth/ProjectAuth.jsx
@@ -4,9 +4,11 @@ import { Link, Button } from "@nextui-org/react";
 
 const ProjectAuth = ({ title, description, imageUrl, link }) => {
   return (
-    <div className="bg-white dark:bg-[#35383a] rounded-3xl shadow-md flex sm:flex-row flex-col sm:gap-10 gap-5 bg-opacity-40 dark:bg-opacity-40">
-      <img src={imageUrl} alt={title} className="rounded-2xl" />
-      <section className='my-auto gap-3 flex flex-col'>
+    <div className=" h-full flex sm:flex-row flex-col sm:gap-10 gap-5">
+      <div className='sm:w-1/2 sm:h-60 flex overflow-hidden bg-[#9ec5e965] dark:bg-[#35383a] bg-opacity-80 rounded-3xl '>
+        <img src={imageUrl} alt={title} className="rounded-2xl transition-all sm:translate-x-10 sm:translate-y-8 sm:hover:translate-x-7 sm:hover:translate-y-7"/>
+      </div>
+      <section className='my-auto gap-3 flex flex-col sm:w-1/2'>
          <h3 className='text-2xl sm:text-4xl text-center sm:text-left font-bold text-slate-800 dark:text-slate-100'>{title}</h3>
          <p className=' font-custom track text-sm sm:text-lg text-slate-600 dark:text-slate-300 max-w-xl rounded-2xl'>{description}</p>
          <Button
